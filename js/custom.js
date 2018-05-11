3b4b205f59d41ad0877e31acf730b3529494811a
Tejas Vaidya <tejasvaidya01@users.noreply.github.com>
2018-05-11 19:53:43 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,55 @@
+!function($) {
+  var mainHeader = $('#header'),
+  		secondaryNavigation = $('.cd-secondary-nav'),
+  		//this applies only if secondary nav is below intro section
+  		belowNavHeroContent = $('.sub-nav-hero'),
+  		headerHeight = mainHeader.height();
+
+  	//set scrolling variables
+  	var scrolling = false,
+  		previousTop = 0,
+  		currentTop = 0,
+  		scrollDelta = 2,
+  		scrollOffset = 100;
+
+  	mainHeader.on('click', '.nav-trigger', function(event){
+  		// open primary navigation on mobile
+  		event.preventDefault();
+  		mainHeader.toggleClass('nav-open');
+  	});
+
+  	$(window).on('scroll', function(){
+  		if( !scrolling ) {
+  			scrolling = true;
+  			(!window.requestAnimationFrame)
+  				? setTimeout(autoHideHeader, 250)
+  				: requestAnimationFrame(autoHideHeader);
+  		}
+  	});
+
+  	$(window).on('resize', function(){
+  		headerHeight = mainHeader.height();
+  	});
+
+  	function autoHideHeader() {
+  		var currentTop = $(window).scrollTop();
+
+  		( belowNavHeroContent.length > 0 )
+  			? checkStickyNavigation(currentTop) // secondary navigation below intro
+  			: checkSimpleNavigation(currentTop);
+
+  	   	previousTop = currentTop;
+  		scrolling = false;
+  	}
+
+  	function checkSimpleNavigation(currentTop) {
+  		//there's no secondary nav or secondary nav is below primary nav
+  	    if (previousTop - currentTop > scrollDelta) {
+  	    	//if scrolling up...
+  	    	mainHeader.removeClass('is-hidden');
+  	    } else if( currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
+  	    	//if scrolling down...
+  	    	mainHeader.addClass('is-hidden');
+  	    }
+  	}
+}(jQuery);
